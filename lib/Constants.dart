259d46d5572a00f508a0de08a7edf33d30b5562e
RotenKiwi <joshiparth212@gmail.com>
2023-02-27 15:17:24 +0530

    post API key commit

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weather/Network/api_response.dart';
 
 const Color textPrimary = Color(0xff313745);
 const Color dayPrimary = Color(0xffe5ecf4);
+
+const String apiKey = 'f0ca071b5ba19a65cd295300a310ea68';
+ApiResponse apiInstance = ApiResponse();
